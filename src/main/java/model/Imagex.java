bb86cfc31a98789027a7d6fa06a209cea09cc971
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-04-08 09:57:38 -0500

    refactorizando clases y variables

---
+++ src/main/java/model/Imagex.java
@@ -0,0 +1,30 @@
+package model;
+
+/**
+ *
+ * @author Luis Miguel
+ */
+public class Imagex {
+    private String id;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    
+
+    
+}
